d205f817eb53d336129d5f83cded066d2facadc4
sergey <mozhov@gmail.com>
2014-07-05 07:14:37 +0100

    Version 1.6. Compatibility with JabRef 2.10. New modifier :regex:str1:str2

--- renamefile/src/renamefile/Utils.java
+++ renamefile/src/renamefile/Utils.java
@@ -3,7 +3,10 @@ package renamefile;
 
 import java.awt.*;
 import java.io.*;
+import java.lang.reflect.Method;
 import java.util.*;
+import java.util.List;
+
 import javax.swing.*;
 
 import net.sf.jabref.*;
@@ -13,12 +16,25 @@ import net.sf.jabref.gui.FileListTableModel;
 import net.sf.jabref.labelPattern.LabelPatternUtil;
 
 public class Utils {
-	private static BasePanel panel;
+	private static JabRefFrame frame;
+	private static Method setDataBase = null;
 
-	public static void init(JabRefFrame frame){
-		panel=frame.basePanel();
+	public static void init(JabRefFrame f){
+		frame=f;
+		Class c;
+		try {
+			c = Class.forName("net.sf.jabref.labelPattern.LabelPatternUtil");
+			Method[] methods = c.getMethods();
+			for (Method m : methods)
+				if (m.getName().equals("setDataBase")) 
+					setDataBase = m;
+		}
+		catch (Exception e) {}
 	}
 
+	private static BasePanel panel(){
+		return frame.basePanel();
+	}
 	public static void addFileEntry(BibtexEntry b, String fn) {
 		if (!hasFile(b,fn)) {
 			FileListTableModel m = new FileListTableModel();
@@ -70,29 +86,54 @@ public class Utils {
 		b.setField("file",m.getStringRepresentation());
 	}
 
+	// code from Metadata.getFileDirectory	
 	public static String getFileDir() {
-		if(panel==null || panel.metaData()==null)
+		if(panel()==null || panel().metaData()==null)
 			return null;
-		MetaData md=panel.metaData();
-		
-//Code from MetaData.getFileDirectory()
+		MetaData md=panel().metaData();
+		File file = md.getFile(); //bib file
+		// Check if the bib file location is set and primary:
+		if (Globals.prefs.getBoolean("bibLocationAsFileDir") 
+				&& Globals.prefs.getBoolean("bibLocAsPrimaryDir")
+				&& file != null) {
+			return file.getParent();
+		}
+		String dir;
 		String key = Globals.prefs.get("userFileDirIndividual");
-        if(md.getData(key)==null)
-//2.6+
-        	key = "fileDirectory";
-//2.7+     	key = Globals.prefs.get("userFileDir");
-        Vector<String> vec = md.getData(key);
-        String dir;
-        if (vec!= null && !vec.isEmpty())
-        	dir = vec.get(0);
-        else
-        	dir = Globals.prefs.get("fileDirectory");
-        if (!(new File(dir)).isAbsolute() && (md.getFile() != null))
-        	dir = new StringBuffer(md.getFile().getParent()).
-        	append(System.getProperty("file.separator")).
-        	append(dir).toString();
-        return dir;
-    }
+		Vector<String> vec = md.getData(key);
+		if (vec == null) {
+			key = Globals.prefs.get("userFileDir");
+			vec = md.getData(key);
+		}
+		if (vec == null)
+			vec = md.getData("fileDirectory"); //2.6?
+		if ((vec != null) && (vec.size() > 0))
+			dir = vec.get(0);
+		else
+			//This is a global file directory
+			//In the original code it is called by get(field+"Directory")
+			dir = Globals.prefs.get("fileDirectory");
+		if (dir==null)
+			dir="";
+		// If this directory is relative, we try to interpret it as relative to
+		// the file path of this bib file:
+		if (!(new File(dir)).isAbsolute() && (file != null)) {
+			String relDir;
+			if (dir.equals(".")) {
+				relDir = file.getParent(); 
+			} else {
+				relDir = new StringBuffer(file.getParent()).
+						append(System.getProperty("file.separator")).
+						append(dir).toString();
+			}
+			// If this directory actually exists, it is very likely that the
+			// user wants us to use it:
+			if ((new File(relDir)).exists())
+				dir = relDir;
+		}
+		return dir;
+	}
+
 
 	public static String getNewFileName(String ofn,BibtexEntry b){
 		boolean sameDir=Globals.prefs.getBoolean("SameFolder");
@@ -140,39 +181,72 @@ public class Utils {
 	}
 	
 	public static String makeLabel(String pattern, BibtexEntry _entry) {
-		String label;
+		// Needed only in 2.10+ as LabelPatternUtil.makeLabel uses the DataBase (private var _db) starting from 2.10:
+		// The public method setDataBase() is defined only starting from 2.10
+		// We look for setDataBase during the initialization
+		try {
+			if (setDataBase!=null)
+					setDataBase.invoke(null, new Object[] {panel().database()});
+		}
+		catch (Exception e) {}
+
+		final String[] bad_chars={"\\\\","/","<",">","\\?","\\{","\\}","\\$","\"","\n",":"};
 		StringBuffer sb = new StringBuffer();
-		String[] bad_chars={"\\\\","/","<",">","\\?","\\{","\\}","\\$","\"","\n",":"};
-		boolean field = false, first =true;
-		for (String i : LabelPatternUtil.split(pattern)) {
-			if (first) {
-				first=false;
-				continue;
-			}
-			if (i.equals("["))
+		String label;
+		boolean field = false;
+		ArrayList<String> lst=LabelPatternUtil.split(pattern);
+		for (int i=1; i<lst.size(); i++) {
+			String s=lst.get(i);
+			if (s.equals("["))
 				field = true;
-			else if (i.equals("]"))
+			else if (s.equals("]"))
 				field = false;
 			else if (field) {
-				String[] parts = LabelPatternUtil.parseFieldMarker(i);
+				String[] parts = LabelPatternUtil.parseFieldMarker(s);
 				String val = parts[0];
 				if (val!=null & val.equals("type")) {
 					BibtexEntryType o = _entry.getType();
 					label = (o!=null ? o.getName():"");
 				} else
 					label = LabelPatternUtil.makeLabel(_entry, val);
-				if (parts.length > 1)
-					label = LabelPatternUtil.applyModifiers(label, parts, 1);
-				for(String s:bad_chars)
-					label=label.replaceAll(s,"");
+				label = applyModifiers(label, parts);
 				sb.append(label);
-			} else {
-				sb.append(i);
-			}
+			} else
+				sb.append(s);
 		}
 		label = sb.toString();
+		for(String c: bad_chars)
+			label=label.replaceAll(c,"");
 		return label;
 	}
+	
+	
+	private static String applyModifiers(String label, String[] parts) {
+		for (int j = 1; j < parts.length; j++) {
+			String modifier = parts[j];
+			if (modifier.equals("lower")) {
+				label = label.toLowerCase();
+			} else if (modifier.equals("upper")) {
+				label = label.toUpperCase();
+			} else if (modifier.equals("abbr")) {
+				StringBuffer abbr = new StringBuffer();
+				String[] words = label.toString().replaceAll("[\\{\\}']","")
+						.split("[\\(\\) \r\n\"]");
+				for (String word: words)
+					if (!word.isEmpty())
+						abbr.append(word.charAt(0));
+				label = abbr.toString();
+			} else if (modifier.equals("regex") && j+2<parts.length) {
+				label = label.replaceAll(parts[j+1],parts[j+2]);
+				j = j + 2;
+			} else {
+				Globals.logger("File name generator warning: unknown modifier '"
+						+ modifier + "'.");
+			}
+		}
+		return label;
+	}
+
 
 	public static boolean copyFile(File of,File nf){
 		InputStream in;
